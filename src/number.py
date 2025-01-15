09bbea88c5351a8635583f5bc580231d806da46b
Sameer747 <sameer.mohsin@live.com>
2025-01-15 22:46:33 +0500

    numbers completed

---
+++ src/number.py
@@ -0,0 +1,30 @@
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+#convert from float to complex:
+d = complex(y)
+
+#convert from int to float:
+e = float(x)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
